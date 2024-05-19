2c6db8093fe60a4f242407e7382fddf42bc3d14e
HM <95107265+hafmohamga@users.noreply.github.com>
2024-05-19 13:18:24 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,20 +1,20 @@
-variable "client_id" {
-  description = "The Client ID (appId) for the Service Principal"
-  type        = string
-}
+#variable "client_id" {
+#  description = "The Client ID (appId) for the Service Principal"
+#  type        = string
+#}
 
-variable "client_secret" {
-  description = "The Client Secret (password) for the Service Principal"
-  type        = string
-  sensitive   = true
-}
+#variable "client_secret" {
+#  description = "The Client Secret (password) for the Service Principal"
+#  type        = string
+#  sensitive   = true
+#}
 
-variable "tenant_id" {
-  description = "The Tenant ID for the Service Principal"
-  type        = string
-}
+#variable "tenant_id" {
+#  description = "The Tenant ID for the Service Principal"
+#  type        = string
+#}
 
-variable "subscription_id" {
-  description = "The Subscription ID for the Azure account"
-  type        = string
-}
+#variable "subscription_id" {
+#  description = "The Subscription ID for the Azure account"
+#  type        = string
+#}
